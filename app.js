816ddb1d188004d72360e8f9e61eef732c7ed546
Jordan Engstrom <jordan.engstrom@gmail.com>
2018-11-15 19:23:51 -0700

    finishing design

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function happyFunction(args) {
-    console.log('This is a function called with ' + args)
-}
-
-happyFunction('meow as an argument')
- 
-window.addEventListener('keydown', function(e){
+function playSound(e) {
     this.console.log(e.keyCode)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    this.console.log(audio)
-    if(!audio) return; // stop the function from running all together
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    //this.console.log(audio)
+    if (!audio) return; // stop the function from running all together
     audio.currentTime = 0; // rewinds to the start
     audio.play();
-});
+    //this.console.log(key)
+    key.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip if it's not a transform
+    this.classList.remove('playing')
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
